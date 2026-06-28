22fe7e6de64c793f691cb6f101c72783f901a9d3
Sidra <chaudharysidra7708@gmail.com>
2026-06-28 11:54:07 +0530

    feat(graph): implement adjacency list graph engine

---
+++ src/Graph.cpp
@@ -0,0 +1,69 @@
+#include "include/Graph.h"
+#include<iostream>
+using namespace std;
+
+Graph::Graph()
+{//-------------------------------------
+    //constructor
+}
+//-------------------------------------
+// check whether a station Exists
+//------------------------------------
+bool Graph::stationExists(const string& stationName ) const
+{
+    return adjacencyList.find(stationName) != adjacencyList.end();
+}
+/*  --------------------------------------------
+    add Station
+---------------------------------------------------   
+*/
+void Graph::addStation(const string& stationName)
+{
+    if(!stationExists(stationName))
+    {
+        adjacencyList[stationName]= {};
+    }
+}
+/*------------------------------------------------------------
+    Add connection between two stations
+------------------------------------------------------------*/
+void Graph::addConnection(
+    const string& source,
+    const string& destination,
+    int distance)
+{
+    //Automatically creates station if they dont exists
+    addStation(source);
+    addStation(destination);
+    //source -> destination
+    adjacencyList[source].push_back({destination,distance});
+    //destination -> source 
+    adjacencyList[destination].push_back({source,distance});
+}
+/*------------------------------------------------------------
+    Display complete metro graph
+------------------------------------------------------------*/
+void Graph::displayGraph() const
+{
+     cout << "\n=========================================\n";
+    cout << "          DELHI METRO GRAPH\n";
+    cout << "=========================================\n\n";
+
+    for(const auto& station : adjacencyList){
+        cout<< station.first<<"\n";
+
+        for (const auto& neighbour : station.second)
+        {
+            cout << "   --> "
+                 << neighbour.first
+                 << " ("
+                 << neighbour.second
+                 << " km)"
+                 << endl;
+        }
+
+        cout << endl;
+    }
+
+    cout << "=========================================\n";
+}
